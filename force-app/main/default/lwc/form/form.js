7a32dfe27ad82a3e8d40b15cbaf9d02660cc0a2f
Matej Đanić <matej.danic@gmail.com>
2021-05-11 18:06:51 +0200

    implement custom games

--- force-app/main/default/lwc/form/form.js
+++ force-app/main/default/lwc/form/form.js
@@ -1,5 +1,7 @@
 import { LightningElement, api, track } from 'lwc';
 import ImageResource from '@salesforce/resourceUrl/ImageResource';
+import rollDice from '@salesforce/apex/GameController.rollDice';
+
 
 export default class Form extends LightningElement {
     @api getForm;
@@ -23,5 +25,16 @@ export default class Form extends LightningElement {
         for (let i=1; i<=6; i++) {
             this.diceImages.push(ImageResource + "/dice/" + i + ".png");
         }
+    }
+
+    handleRollDice() {
+        rollDice({
+            gameId: "a0009000008HOgLAAW",
+            diceList: [1, 2, 3, 4, 5]
+        }).then(diceList => {
+            console.log(diceList);
+        }).catch(error => {
+            console.log("Error (getGameById):", error);
+        });
     }
 }
