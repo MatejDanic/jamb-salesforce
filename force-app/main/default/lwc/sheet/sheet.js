63f255b3a56fef2530e23ebbd1d9d3bde3b6f554
Matej Đanić <matej.danic@triple-innovations.com>
2023-07-04 13:16:09 +0200

    fix roll button

--- force-app/main/default/lwc/sheet/sheet.js
+++ force-app/main/default/lwc/sheet/sheet.js
@@ -66,13 +66,13 @@ export default class Sheet extends LightningElement {
 
 	// determines if top/middle/bottom lightning bolt in the roll dice button will be filled or not
 	get rollCountTopClass() {
-		return this.sheet.rollCount == 0 ? "roll-count-filled" : "";
+		return this.sheet.rollCount == 0 ? "filled" : "empty";
 	}
 	get rollCountMiddleClass() {
-		return this.sheet.rollCount <= 1 ? "roll-count-filled" : "";
+		return this.sheet.rollCount <= 1 ? "filled" : "empty";
 	}
 	get rollCountBottomClass() {
-		return this.sheet.rollCount <= 2 ? "roll-count-filled" : "";
+		return this.sheet.rollCount <= 2 ? "filled" : "empty";
 	}
 
 	// handler functions
